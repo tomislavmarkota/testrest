3b26e0c31d523464f418147b38f1a47401f9ad5c
unknown <tomislav.markota11@gmail.com>
2022-11-18 21:59:35 +0100

    first commit

---
+++ routes/feed.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const feedController = require('../controllers/feed');
+const router = express.Router();
+
+
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+router.post('/add-posts', feedController.createPost);
+
+module.exports = router;
